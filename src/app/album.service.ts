64ae724af9e66bec4061a1880c4fa8d2ffe6f9be
Guest User <Guest@Epicodus-5A.local>
2018-04-02 15:56:12 -0700

    hamza fixed it

--- src/app/album.service.ts
+++ src/app/album.service.ts
@@ -1,15 +1,19 @@
 import { Injectable } from '@angular/core';
 import { Album } from './album.model';
 import { ALBUMS } from './mock-albums';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+
 
 @Injectable()
 export class AlbumService {
-
-  constructor() { }
-
-  getAlbums() {
-    return ALBUMS;
+  albums: FirebaseListObservable<any[]>;
+  constructor(private database: AngularFireDatabase) {
+    this.albums = database.list('albums');
   }
+
+  getAlbums(){
+return this.albums;
+}
 
   getAlbumById(albumId: number){
     for (var i = 0; i <= ALBUMS.length - 1; i++) {
